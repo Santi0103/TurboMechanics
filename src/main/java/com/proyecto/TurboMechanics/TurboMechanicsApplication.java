1719bfb9193028c0ade01936aeeddcf2c5b52ddd
Kevin03 <santimosquerav@gmail.com>
2026-04-20 15:30:26 -0500

    feat: creation repository

---
+++ src/main/java/com/proyecto/TurboMechanics/TurboMechanicsApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto.TurboMechanics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurboMechanicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TurboMechanicsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/proyecto/TurboMechanics/TurboMechanicsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto.TurboMechanics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TurboMechanicsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
