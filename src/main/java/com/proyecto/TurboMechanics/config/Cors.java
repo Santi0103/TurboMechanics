12be399b6b380250b5905611120f6e58cc81da10
19Juanjo <juanmuno1904@gmail.com>
2026-04-27 17:04:11 -0500

    fix: correction in the cors and filter config

--- src/main/java/com/proyecto/TurboMechanics/config/Cors.java
+++ src/main/java/com/proyecto/TurboMechanics/config/Cors.java
@@ -1,7 +1,9 @@
 package com.proyecto.TurboMechanics.config;
 
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
@@ -9,7 +11,7 @@ import org.springframework.web.filter.CorsFilter;
 @Configuration
 public class Cors {
     @Bean
-    public CorsFilter corsFilter() {
+    public FilterRegistrationBean<CorsFilter> corsFilter() {
         CorsConfiguration config = new CorsConfiguration();
         config.addAllowedOrigin("http://localhost:4200");
         config.addAllowedMethod("*");
@@ -19,6 +21,8 @@ public class Cors {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", config);
 
-        return new CorsFilter(source);
+        FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(new CorsFilter(source));
+        bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        return bean;
     }
 }
